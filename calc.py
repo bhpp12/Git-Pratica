2cc3d3a78a71b296de8b646e258a785e11dfb55b
Bruno Henrique Pacheco Prudêncio <brunohenriquepp12@gmail.com>
2026-09-24 16:17:10 -0300

    Bug fix divisão por zero na função div() da calculadora.

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
-def sub(x, y):
-    return x - y
-
 def sum(x, y):
-    return x + y
+  return x + y
+  
+def sub(x, y): 
+  return x - y
 
 def div(x, y):
-    return x / y
+  return x / y if y != 0 else None
